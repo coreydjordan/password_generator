4e69488109e22dcd7e6acb2672582741a4c4f416
coreyjordanphx <corey.d.jordan66@gmail.com>
2022-04-22 14:26:32 -0500

    added base, home, about, and password templates

--- pwdgenerator/generator/views.py
+++ pwdgenerator/generator/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import string
+import random
 # Create your views here.
+
+def home(request):
+    # return HttpResponse('hello world')
+    return render(request, 'generator/home.html')
+
+def password(request):
+    chars = list(string.ascii_lowercase)
+    
+    if request.GET.get('uppercase'):
+        chars.extend(list(string.ascii_uppercase))
+    
+    if request.GET.get('numbers'):
+        chars.extend(list(string.digits))
+        
+    if request.GET.get('special'):
+        chars.extend(list('!@#$%^&*()'))
+    
+    length = int(request.GET.get('length'))
+    
+    pwd = ''
+    
+    for i in range(length):
+        pwd += random.choice(chars)
+    return render(request, 'generator/password.html', {'password': pwd})
